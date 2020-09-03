56b10c9904cabad0e313f5134d5b2131d53bfa5e
the-it-weirdo <ddsmegh4@gmail.com>
2020-09-03 17:17:29 +0530

    Removes api endpoints and key

--- app/src/main/java/dev/debaleen/foodrunner/util/Constants.kt
+++ app/src/main/java/dev/debaleen/foodrunner/util/Constants.kt
@@ -1,16 +1,5 @@
 package dev.debaleen.foodrunner.util
 
-const val NETWORK_IP = "http://13.235.250.119/v2/"
-const val REGISTER = "$NETWORK_IP/register/fetch_result"
-const val LOGIN = "$NETWORK_IP/login/fetch_result"
-const val FORGOT_PASSWORD = "$NETWORK_IP/forgot_password/fetch_result"
-const val RESET_PASSWORD = "$NETWORK_IP/reset_password/fetch_result"
-const val FETCH_RESTAURANTS = "$NETWORK_IP/restaurants/fetch_result"
-const val FETCH_RESTAURANT_DETAILS = "$NETWORK_IP/restaurants/fetch_result/"
-const val PLACE_ORDER = "$NETWORK_IP/place_order/fetch_result"
-const val FETCH_PREVIOUS_ORDERS = "$NETWORK_IP/orders/fetch_result/"
-const val TOKEN = "141f8efeca1968"
-
 const val isLoggedInKey = "isLoggedIn"
 const val userIdKey=  "userId"
 const val userMobileKey = "userMobile"
